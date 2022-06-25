2f688d1243233b2891738727f30da5a82470f05e
Michael Shen <michael7105@gmail.com>
2022-06-25 13:10:57 +0000

    indicate bash

--- renew_cert.sh
+++ renew_cert.sh
@@ -5,9 +5,5 @@
 # 2021/05/18    nxshen  First release
 PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
 
-docker stop $(docker ps -aq)
-docker system prune -f
-
-
-docker rmi $(docker images -q)
-docker volume rm $(docker volume ls -q)
+certbot renew
+cp /etc/letsencrypt/archive/michaelshen.csie.io/* ./apache/ssl/
